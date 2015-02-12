ec6943698d3a2fbc6c66aaef3bed64ef036f68f2
Drew Nichols <drew@liftforward.com>
2015-02-12 12:51:58 -0500

    adding rspec filter :focus and default run all when no focus set

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,11 @@ unless defined?(SPEC_HELPER_LOADED)
 
   require "yodlicious"
 
+
+  RSpec.configure do |config|
+
+    config.filter_run :focus
+    config.run_all_when_everything_filtered = true
+
+  end
 end
