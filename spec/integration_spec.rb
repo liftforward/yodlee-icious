74ee8363a1d636c2391e05b6330ec16d6d16236a
Drew Nichols <drew@liftforward.com>
2015-02-17 13:22:23 -0500

    fixing up integration tests

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -67,34 +67,45 @@ describe 'the yodlee api client integration test', integration: true do
   describe 'the yodlee apis register user endpoint' do
     context 'Given a valid cobranded credentials and base_url' do
       context 'Given a new user who does not exist within the cobranded account' do
-        context 'When /jsonsdk/UserRegistration/register3 endpoint is called' do
-          describe 'the response' do
-            subject {
-              api.cobranded_login
-              api.user_login 'testuser', 'testpassword143'
-              api.unregister_user
-              api.register_user 'testuser', 'testpassword143', 'test@test.com'
-            }
+        context 'When /jsonsdk/UserRegistration/register3 endpoint is called the response' do
+          subject {
+            api.cobranded_login
+            api.register_user "testuser#{rand(100...200)}", 'testpassword143', 'test@test.com'
+          }
 
-            it { is_expected.to be_kind_of(Hash) }
-            it { is_expected.not_to be_empty }
+          after {
+            api.unregister_user
+          }
 
-            it 'is expected to not return an error' do
-              expect(subject['errorOccurred']).to be_nil
-            end
-
-            it 'is expected to contain a new sessionToken' do
-              #{"userContext":{"conversationCredentials":{"sessionToken":"12162013_1:ca362b46803f7ff4ea73e413a0341b6cdf895f51831d01856bfd4daaa5f816810b36252569cd7b4fd8e115b00c674e7162d01a008802c10aec3af48096e74005"},"valid":true,"isPasswordExpired":false,"cobrandId":15910008380,"channelId":-1,"locale":"en_US","tncVersion":2,"applicationId":"270772870CDC1786FFEDECFDA1FDBC00","cobrandConversationCredentials":{"sessionToken":"12162013_1:55a575777b5509749442b93651ca1400936d3562e30f82fbfb561f29abab7e129086259e1ac30d0247f5fbc25aed9752fb9bd287e7ccedbb65dfc8ec98841cc6"},"preferenceInfo":{"currencyCode":"USD","timeZone":"PST","dateFormat":"MM/dd/yyyy","currencyNotationType":{"currencyNotationType":"SYMBOL"},"numberFormat":{"decimalSeparator":".","groupingSeparator":",","groupPattern":"###,##0.##"}}},"lastLoginTime":1424115740,"loginCount":0,"passwordRecovered":false,"emailAddress":"test@test.com","loginName":"testuser","userId":19843605,"isConfirmed":false} headers={"x-powered-by"=>"Unknown", "set-cookie"=>"JSESSIONID=A3099AF04A71E6237973B2AAC7B21F8F; Path=/yodsoap; Secure", "content-type"=>"application/json", "vary"=>"Accept-Encoding", "date"=>"Mon, 16 Feb 2015 19:42:20 GMT", "connection"=>"close", "server"=>"Unknown"}
-              expect(subject['userContext']['conversationCredentials']['sessionToken']).to be_kind_of(String)
-              expect(subject['userContext']['conversationCredentials']['sessionToken'].length).to be > 0
-
-              expect(api.user_session_token).not_to be_nil
-            end
-
-            after {
-              api.unregister_user
-            }
+          it 'is expected to offer a valid response' do
+            is_expected.to be_kind_of(Hash)
+            is_expected.not_to be_empty
+            expect(subject['errorOccurred']).to be_nil
+            expect(subject['userContext']['conversationCredentials']['sessionToken']).to be_kind_of(String)
+            expect(subject['userContext']['conversationCredentials']['sessionToken'].length).to be > 0
+            expect(api.user_session_token).not_to be_nil
           end
+        end
+      end
+    end
+  end
+
+  describe 'the yodlee apis site info endpoint' do
+    context 'Given a valid cobranded credentials and base_url' do
+      before { 
+        api.cobranded_login
+      }
+
+      context 'When a request for site info is performed the result' do
+        subject { api.get_site_info 16441 }
+
+        it 'is expected to contain login form details' do
+          is_expected.not_to be_nil
+          is_expected.to be_kind_of(Hash)
+          expect(subject['errorOccurred']).to be_nil
+          expect(subject['loginForms']).not_to be_nil
+          expect(subject['loginForms']).to be_kind_of(Array)
+          expect(subject['loginForms'].length).to be > 0
         end
       end
     end
@@ -118,98 +129,85 @@ describe 'the yodlee api client integration test', integration: true do
         end
       }
 
-      let(:chase_login_form) {
-        {
-          conjunctionOp: {  
-            conjuctionOp: 1
-          },
-          componentList: [  
-            {  
-              valueIdentifier: 'LOGIN',
-              valueMask: 'LOGIN_FIELD',
-              fieldType: {  
-                typeName: 'IF_LOGIN'
-              },
-              size: 20,
-              maxlength: 32,
-              name: 'LOGIN',
-              displayName: 'User ID',
-              isEditable: true,
-              isOptional: false,
-              isEscaped: false,
-              helpText: 4710,
-              isOptionalMFA: false,
-              isMFA: false,
-              value: 'kanyewest'
-            },
-            {  
-              valueIdentifier: 'PASSWORD',
-              valueMask: 'LOGIN_FIELD',
-              fieldType: {  
-                typeName: 'IF_PASSWORD'
-              },
-              size: 20,
-              maxlength: 40,
-              name: 'PASSWORD',
-              displayName: 'Password',
-              isEditable: true,
-              isOptional: false,
-              isEscaped: false,
-              helpText: 11976,
-              isOptionalMFA: false,
-              isMFA: false,
-              value: 'ILoveTheGrammys'
-            }
-          ],
-          defaultHelpText: 324
-        }
-      }
-
       context 'When a invalid username and password for an account is added' do
-        subject { api.add_site_account_and_wait(643, chase_login_form) }
+        before {
+          dag_login_form[:componentList][0][:value] = 'invalid_username'
+          dag_login_form[:componentList][1][:value] = 'invalid_password'
+        }
+        subject { api.add_site_account_and_wait(16441, dag_login_form, 1) }
 
-        it 'is expected to respond with siteRefreshStatus=LOGIN_FAILURE and refreshMode=NORMAL a siteAccountId' ,focus: true do
+        it 'is expected to respond with siteRefreshStatus=LOGIN_FAILURE and refreshMode=NORMAL a siteAccountId' do
           expect(subject['siteRefreshInfo']['siteRefreshStatus']['siteRefreshStatus']).to eq('LOGIN_FAILURE')
           expect(subject['siteRefreshInfo']['siteRefreshMode']['refreshMode']).to eq('NORMAL')
           expect(subject['siteAccountId']).not_to be_nil
           # puts JSON.pretty_generate(subject)
         end
       end
+
+      context 'When a valid username and password for an account is added' do
+        before {
+          dag_login_form[:componentList][0][:value] = 'yodlicious.site16441.1'
+          dag_login_form[:componentList][1][:value] = 'site16441.1'
+        }
+        subject { api.add_site_account_and_wait(16441, dag_login_form, 1) }
+
+        it 'is expected to respond with siteRefreshStatus=LOGIN_SUCCESS and refreshMode=NORMAL a siteAccountId' do
+          # puts JSON.pretty_generate(subject)
+
+          expect(subject['siteRefreshInfo']['siteRefreshStatus']['siteRefreshStatus']).to eq('LOGIN_SUCCESS')
+          expect(subject['siteRefreshInfo']['siteRefreshMode']['refreshMode']).to eq('NORMAL')
+          expect(subject['siteAccountId']).not_to be_nil
+        end
+      end
+
     end
   end
 
-  describe 'the yodlee apis fetching summary data about registered accounts endpoints' do
+  describe 'the yodlee apis fetching summary data about registered site accounts endpoints' do
     context 'Given a registered user with registered accounts' do
       before { 
         api.cobranded_login
-        api.user_login 'sbMemandrewnichols2', 'sbMemandrewnichols2#123'
+        api.user_login "testuser_with_transactions", 'testpassword143'
+        # api.register_user "testuser#{rand(100..999)}", 'testpassword143', 'test@test.com'
+        # dag_login_form[:componentList][0][:value] = 'yodlicious.site16441.1'
+        # dag_login_form[:componentList][1][:value] = 'site16441.1'
+        # api.add_site_account_and_wait(16441, dag_login_form)
       }
+
+      # after {
+      #   begin
+      #     api.unregister_user
+      #   rescue
+      #   end
+      # }
 
       context 'when getAllSiteAccounts is called the return' do 
         subject { api.get_all_site_accounts }
 
-        it { is_expected.to be_kind_of(Array) }
-        it { is_expected.not_to be_nil }
-        it 'is expected to return more than 0 sites' do
+        it 'is expected to return an array containing 1 siteAccount' do
+          # puts JSON.pretty_generate(subject)
+          is_expected.not_to be_nil
+          is_expected.to be_kind_of(Array)
           expect(subject.length).to be > 0
-        end
-        it 'is expected to return a siteAccountId' do
           expect(subject[0]['siteAccountId']).not_to be_nil
+          expect(subject[0]['siteRefreshInfo']['siteRefreshStatus']['siteRefreshStatus']).to eq('REFRESH_COMPLETED')
+
         end
       end
 
       context 'when getItemSummariesForSite is called the return' do
         subject {
-          site_account_id = api.get_all_site_accounts[0]['siteAccountId']
-          api.get_item_summaries_for_site(site_account_id)
+          site_accounts = api.get_all_site_accounts
+          # puts site_accounts[0]['siteAccountId']
+          # puts JSON.pretty_generate(site_accounts)
+          api.get_item_summaries_for_site(site_accounts[0]['siteAccountId'])
         }
 
-        it { is_expected.not_to be_nil }
-        it { is_expected.to be_kind_of(Array) }
-        it 'is expected to return more than 0 sites' do
-          expect(subject.length).to be > 0
-        end
-        it 'is expected to return an itemId' do
+        it 'is expected to return an array site summaries' do
+          # puts JSON.pretty_generate(subject)
+
+          is_expected.not_to be_nil
+          is_expected.to be_kind_of(Array)
           expect(subject[0]['itemId']).not_to be_nil
         end
       end
@@ -217,11 +215,13 @@ describe 'the yodlee api client integration test', integration: true do
       context 'when getItemSummaries is called the return' do
         subject { api.get_item_summaries }
 
-        it { is_expected.not_to be_nil }
-        it { is_expected.to be_kind_of(Array) }
+        it 'is expected to return an array of site summaries' do
+          # puts JSON.pretty_generate(subject)
 
-        it 'is expected to return more than 0 sites' do
+          is_expected.not_to be_nil
+          is_expected.to be_kind_of(Array)
           expect(subject.length).to be > 0
+          expect(subject[0]['itemId']).not_to be_nil
         end
       end
     end
@@ -231,23 +231,22 @@ describe 'the yodlee api client integration test', integration: true do
     context 'Given a registered user with registered accounts' do
       before { 
         api.cobranded_login
-        api.user_login 'sbMemandrewnichols2', 'sbMemandrewnichols2#123'
+        api.user_login "testuser_with_transactions", 'testpassword143'
+        # dag_login_form[:componentList][0][:value] = 'yodlicious.site16441.1'
+        # dag_login_form[:componentList][1][:value] = 'site16441.1'
+        # api.add_site_account_and_wait(16441, dag_login_form)
       }
 
       context 'When a transaction search for all transactions is performed the result' do
         subject { api.execute_user_search_request }
 
-        it { is_expected.to be_kind_of(Hash) }
-        it 'is expected to not contain an error' do
+        it 'is expected to return a valid search result' do
+          # puts JSON.pretty_generate(subject)
+
+          is_expected.not_to be_nil
+          is_expected.to be_kind_of(Hash)
           expect(subject['errorOccurred']).to be_nil
-        end
-        it { is_expected.not_to be_nil }
-
-        it 'is expected to contain a searchIdentifier' do
           expect(subject['searchIdentifier']).not_to be_nil
-        end
-
-        it 'is expected to contain an array of transactions larger then 0' do
           expect(subject['searchResult']['transactions']).to be_kind_of(Array)
           expect(subject['searchResult']['transactions'].length).to be > 0
         end
@@ -255,36 +254,6 @@ describe 'the yodlee api client integration test', integration: true do
     end
   end
 
-  describe 'the yodlee apis site info' do
-    context 'Given a registered cobranded session' do
-      before { 
-        api.cobranded_login
-        # api.user_login 'sbMemandrewnichols2', 'sbMemandrewnichols2#123'
-      }
-
-      context 'When a request for site info is performed the result' do
-        subject { api.get_site_info 643 }
-
-        it { is_expected.to be_kind_of(Hash) }
-        it 'is expected to not contain an error' do
-          puts JSON.pretty_generate subject
-          expect(subject['errorOccurred']).to be_nil
-        end
-        it { is_expected.not_to be_nil }
-
-        it 'is expected to contain login form details' do
-          expect(subject['loginForms']).not_to be_nil
-          expect(subject['loginForms']).to be_kind_of(Array)
-          expect(subject['loginForms'].length).to be > 0
-        end
-
-        # it 'is expected to contain an array of transactions larger then 0' do
-        #   expect(subject['searchResult']['transactions']).to be_kind_of(Array)
-        #   expect(subject['searchResult']['transactions'].length).to be > 0
-        # end
-      end
-    end
-  end
 
   # puts JSON.pretty_generate(subject)
 
@@ -361,4 +330,95 @@ describe 'the yodlee api client integration test', integration: true do
           #   "retryCount"=>0
           # }
 
+
+      #   {
+      #     conjunctionOp: {  
+      #       conjuctionOp: 1
+      #     },
+      #     componentList: [  
+      #       {  
+      #         valueIdentifier: 'LOGIN',
+      #         valueMask: 'LOGIN_FIELD',
+      #         fieldType: {  
+      #           typeName: 'IF_LOGIN'
+      #         },
+      #         size: 20,
+      #         maxlength: 32,
+      #         name: 'LOGIN',
+      #         displayName: 'User ID',
+      #         isEditable: true,
+      #         isOptional: false,
+      #         isEscaped: false,
+      #         helpText: 4710,
+      #         isOptionalMFA: false,
+      #         isMFA: false,
+      #         value: 'yodlicious.site16441.1'
+      #       },
+      #       {  
+      #         valueIdentifier: 'PASSWORD',
+      #         valueMask: 'LOGIN_FIELD',
+      #         fieldType: {  
+      #           typeName: 'IF_PASSWORD'
+      #         },
+      #         size: 20,
+      #         maxlength: 40,
+      #         name: 'PASSWORD',
+      #         displayName: 'Password',
+      #         isEditable: true,
+      #         isOptional: false,
+      #         isEscaped: false,
+      #         helpText: 11976,
+      #         isOptionalMFA: false,
+      #         isMFA: false,
+      #         value: 'site16441.1'
+      #       }
+      #     ],
+      #     defaultHelpText: 324
+      #   }
+      # }
+
+  let(:dag_login_form) {
+    JSON.parse('{
+      "conjunctionOp": {
+        "conjuctionOp": 1
+      },
+      "componentList": [
+        {
+          "valueIdentifier": "LOGIN1",
+          "valueMask": "LOGIN_FIELD",
+          "fieldType": {
+            "typeName": "IF_LOGIN"
+          },
+          "size": 20,
+          "maxlength": 40,
+          "name": "LOGIN1",
+          "displayName": "Catalog",
+          "isEditable": true,
+          "isOptional": false,
+          "isEscaped": false,
+          "helpText": "150862",
+          "isOptionalMFA": false,
+          "isMFA": false
+        },
+        {
+          "valueIdentifier": "PASSWORD1",
+          "valueMask": "LOGIN_FIELD",
+          "fieldType": {
+            "typeName": "IF_PASSWORD"
+          },
+          "size": 20,
+          "maxlength": 40,
+          "name": "PASSWORD1",
+          "displayName": "Password",
+          "isEditable": true,
+          "isOptional": false,
+          "isEscaped": false,
+          "helpText": "150863",
+          "isOptionalMFA": false,
+          "isMFA": false
+        }
+      ],
+      "defaultHelpText": "16103"
+    }', symbolize_names: true)
+  }
 end
