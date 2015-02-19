080f18c7342e0b1f4b311c5760ba00ced4b0ba4d
Drew Nichols <drew@liftforward.com>
2015-02-19 10:57:26 -0500

    removing unused config_spec

--- spec/config_spec.rb
+++
@@ -1,9 +0,0 @@
-require "yodlicious/config"
-
-# describe Yodlicious::Config do
-#   it 'should return config values after being set' do
-#     Yodolicious::Config.base_url = 'blablabla'
-
-#     expect(Yodolicious::Config.base_url).to eq('blablabla')
-#   end
-# end
