265fa8460d4928eda271a093d1970609f558c006
Drew Nichols <drew@liftforward.com>
2015-08-10 10:54:06 -0400

    trying to fix codeclimate coverage report

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,13 @@
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require 'codeclimate-test-reporter'
+  CodeClimate::TestReporter.start
+end
+
 unless defined?(SPEC_HELPER_LOADED)
   SPEC_HELPER_LOADED = true
 
   require "yodlicious"
   require 'dotenv'
-  require "codeclimate-test-reporter"
-  
-  CodeClimate::TestReporter.start
   Dotenv.load
 
   RSpec.configure do |config|
