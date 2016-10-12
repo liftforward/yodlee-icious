30791474a6e850488149bc7ed9fd6dd6637f7f4a
Drew Nichols <drew@liftforward.com>
2016-10-12 15:11:55 -0400

    fixing response unit test to work

--- spec/unit/yodleeicious/response_spec.rb
+++ spec/unit/yodleeicious/response_spec.rb
@@ -1,48 +1,41 @@
 require "yodleeicious"
 
-describe Yodleeicious::Response do
-  let(:error_response_1) {
-    {
-      "errorOccurred"=>"true",
-      "exceptionType"=>"com.yodlee.core.IllegalArgumentValueException",
-      "referenceCode"=>"_3932d208-345a-400f-a273-83619b8b548b",
-      "message"=>"Multiple exceptions encapsulated within: invoke getWrappedExceptions for details"
-    }
-  }
-
-  let(:error_response_2) {
-    { "Error" => [ {"errorDetail" => "Invalid User Credentials"} ] }
-  }
-
-  let(:success_hash_response) {
-    {}
-  }
-
-  let(:success_array_response) {
-    [{}]
-  }
-
-  shared_examples 'sets_the_requests' do
-    it 'sets the response data' do
-      expect(subject.response).not_to be_nil
-    end
-
-    it 'sets the request_url' do
-      expect(subject.request_url).not_to be_nil
-    end
-
-    it 'sets payload' do
-      expect(subject.payload).not_to be_nil
-    end
-
-    it 'sets the status code' do
-      expect(subject.status).not_to be_nil
-    end
+shared_examples 'sets_the_requests' do
+  it 'sets the response data' do
+    expect(subject.response).not_to be_nil
   end
 
+  it 'sets the request_url' do
+    expect(subject.request_url).not_to be_nil
+  end
+
+  it 'sets payload' do
+    expect(subject.payload).not_to be_nil
+  end
+
+  it 'sets the status code' do
+    expect(subject.status).not_to be_nil
+  end
+end
+
+
+describe Yodleeicious::Response do
+  let (:mock_env) { double("env", url: "http://example.com") }
+  let (:response) { double("Response", body: response_json, env: mock_env, status: 200 ) }
+
+  subject { Yodleeicious::Response.new response, { key: "val" } }
 
   context 'When the error_response is the errorOccured syntax' do
-    subject { Yodleeicious::Response.new error_response_1 }
+    let(:response_json) {
+      {
+        "errorOccurred"=>"true",
+        "exceptionType"=>"com.yodlee.core.IllegalArgumentValueException",
+        "referenceCode"=>"_3932d208-345a-400f-a273-83619b8b548b",
+        "message"=>"Multiple exceptions encapsulated within: invoke getWrappedExceptions for details"
+      }.to_json
+    }
+
+    include_examples "sets_the_requests"
     it { is_expected.not_to be_success }
     it { is_expected.to be_fail }
     it "is expected to return error of InvalidArgumentValueException" do
@@ -51,7 +44,13 @@ describe Yodleeicious::Response do
   end
 
   context 'When the error_response is the Error : ["errorDetail"] syntax' do
-    subject { Yodleeicious::Response.new error_response_2 }
+    let(:response_json) { { "Error" => [ {"errorDetail" => "Invalid User Credentials"} ] }.to_json }
+    let (:mock_env) { double("env", url: "http://example.com") }
+    let (:response) { double("Response", body: response_json, env: mock_env, status: 200 ) }
+
+    subject { Yodleeicious::Response.new response, { key: "val" } }
+
+    include_examples "sets_the_requests"
     it { is_expected.not_to be_success }
     it { is_expected.to be_fail }
     it "is expected to return error of Invalid User Credentials" do
@@ -60,7 +59,11 @@ describe Yodleeicious::Response do
   end
 
   context 'When operation is a success and returns hash' do
-    subject { Yodleeicious::Response.new success_hash_response }
+    let(:response_json) { {}.to_json }
+
+    subject { Yodleeicious::Response.new response, { key: "val" } }
+
+    include_examples "sets_the_requests"
     it { is_expected.to be_success }
     it { is_expected.not_to be_fail }
     it 'is expected to return nil for error' do
@@ -69,7 +72,11 @@ describe Yodleeicious::Response do
   end
 
   context 'When operation is a success and return array' do
-    subject { Yodleeicious::Response.new success_array_response }
+    let(:response_json) { [{}].to_json }
+
+    subject { Yodleeicious::Response.new response, { key: "val" } }
+
+    include_examples "sets_the_requests"
     it { is_expected.to be_success }
     it { is_expected.not_to be_fail }
     it 'is expected to return nil for error' do
